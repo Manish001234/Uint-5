fcccb183da0820b19eacb54df5f59ef02488c8cc
Manish001234 <manishreddy621@gmail.com>
2022-03-22 23:39:09 +0530

    4

---
+++ Day-4/day-4/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import {useState} from "react"
+import { Grocery } from './components/grosery'
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+        <Grocery/>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ Day-4/day-4/src/components/grosery.jsx
@@ -0,0 +1,10 @@
+import {useState} from "react"
+import {GroceryInput} from "./groseryInput" 
+const Grocery = () =>{
+
+    return (
+
+        <GroceryInput/>
+    )
+}
+export {Grocery}
